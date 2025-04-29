1e9419540e139499457706c3d65ba5b97f45ae63
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2025-04-29 21:52:02 +0600

    added header

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import logo from "../../assets/logo.png";
+import profile from "../../assets/profile.png";
 
 const Header = () => {
     return (
         <div>
-            <h3>This is header</h3>
+            <div className='bg-[#ED6923] text-center py-3'>
+                <h3 className='text-white'> Free Delivery on Your First Order – Order Now!</h3>
+            </div>
+            <div className='bg-white py-4 shadow'>
+                <div className='flex justify-between max-w-[1200px] mx-auto px-4'>
+                    <div>
+                        <img src={logo} alt="" />
+                    </div>
+                    <div>
+                        <img src={profile} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
